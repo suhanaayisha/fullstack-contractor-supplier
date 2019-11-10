2a1bdd031f3406caa5f0d89028f77b89dd484abf
suhana <suhana.ayisha@dubizzle.com>
2019-11-10 17:29:13 +0400

    users not data

--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -6,7 +6,7 @@ export default class SignUp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: [],
+            users: [],
             username: null,
             password: null,
             usertype: null,
