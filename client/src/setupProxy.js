4cef7149290ddc8d6c8bf86ded2a29966fe2fcd5
suhana <suhana.ayisha@dubizzle.com>
2019-11-10 18:28:19 +0400

    setting origin to true

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,17 @@
 const proxy = require("http-proxy-middleware");
 
+// module.exports = function(app) {
+//   app.use(
+//     proxy(["/api", , "/otherApi"], { target: "http://localhost:3001" })
+//   );
+// };
+
 module.exports = function(app) {
-  app.use(
-    proxy(["/api", , "/otherApi"], { target: "http://localhost:3001" })
-  );
-};
+    app.use(
+      '/api',
+      proxy({
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      })
+    );
+  };
