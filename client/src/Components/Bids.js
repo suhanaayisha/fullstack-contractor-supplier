d87ed9147b6da13acbcfcb349de0da558a888468
suhana <suhana.ayisha@dubizzle.com>
2019-11-11 16:10:06 +0400

    data getting passed

--- client/src/Components/Bids.js
+++ client/src/Components/Bids.js
@@ -6,7 +6,8 @@ export default class Supplier extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [],
+      bidData: [],
+      reqData: [],
       userid: this.props.location.userDetails.userid,
       username: this.props.location.userDetails.username,
       intervalIsSet: false
@@ -17,7 +18,8 @@ export default class Supplier extends React.Component {
       
 
     componentDidMount() {
-        this.getDataFromDb();
+        this.getDataBidFromDb();
+        this.getReqDataFromDb();
         if (!this.state.intervalIsSet) {
           let interval = setInterval(this.getDataFromDb, 1000);
           this.setState({ intervalIsSet: interval });
@@ -30,10 +32,16 @@ export default class Supplier extends React.Component {
         }
       }
 
-    getDataFromDb = () => {
+    getBidDataFromDb = () => {
         fetch('./api/getBidData')
           .then((data) => data.json())
-          .then((res) => this.setState({ data: res.data }));
+          .then((res) => this.setState({ bidData: res.data }));
+      };
+
+    getReqDataFromDb = () => {
+        fetch('./api/getRequestData')
+          .then((data) => data.json())
+          .then((res) => this.setState({ reqData: res.data }));
       };
 
       
@@ -42,7 +50,7 @@ export default class Supplier extends React.Component {
         console.log(this.state.username)
         console.log(this.state.userid)
         console.log("no")
-        const { data } = this.state;
+        const { bidData } = this.state;
         const userid = this.state.userid;
         const username = this.state.username;
         return(
@@ -56,12 +64,24 @@ export default class Supplier extends React.Component {
                 <NavLink to='/' activeClassName='is-active' exact={true}>Logout</NavLink> 
                 <h1>Bids Placed</h1>
                 <ul>
-                {data.length <= 0
+                {bidData.length <= 0
                     ? 'No Bids yet'
-                    : data.map((dat) => (
+                    : bidData.map((dat) => (
                         <li style={{ padding: '10px' }} key={dat.id}>
                         <span style={{ color: 'gray' }}> Bid Id: </span> {dat._id} <br />
                         <span style={{ color: 'gray' }}> Request Id </span> {dat.reqid} <br />
+                        { reqData.map((data) => {
+                            if(data._id === dat.reqid ){
+                              return(
+                                <li>
+                                  <span style={{ color: 'gray' }}> Requested Equipment: </span> {data.equip} <br />
+                                  <span style={{ color: 'gray' }}> Requested Quantity </span> {data.quantity} <br />
+                                </li>
+                              )
+                            }
+                        })
+
+                        }
                         <span style={{ color: 'gray' }}> Price </span>  {dat.price} <br />
                         </li>
                     ))}
